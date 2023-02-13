02feeed78f790f4aabb5c85b7c3c6e3c81c90752
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-02-13 07:15:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,9 +42,9 @@ class evamaria(Client):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID','669072'))
-API_HASH = environ.get('API_HASH','3f6835286b03e000ab6d71b37cc35b92')
-BOT_TOKEN = environ.get('BOT_TOKEN','5510874505:AAFlN6GOFKJEFpK_CM7QbBSGAqcf7AFCzNo')
+API_ID = int(environ.get('API_ID', 669072))
+API_HASH = environ.get('API_HASH', 3f6835286b03e000ab6d71b37cc35b92)
+BOT_TOKEN = environ.get('BOT_TOKEN', 5510874505:AAFlN6GOFKJEFpK_CM7QbBSGAqcf7AFCzNo)
 
 # Bot settings
 CACHE_TIME = 300
